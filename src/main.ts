87fc2e2ce65641797235b5b292b0dfd83b9b8d86
Radha743 <worldofnara777@gmail.com>
2024-01-03 17:01:46 +0530

    Rendeer

--- src/main.ts
+++ src/main.ts
@@ -34,13 +34,34 @@ container.appendChild(canvas.domElement);
 	basicShader.bind();
 	va.bind();
 
-	const draw = () => {
-		gl.clearColor(0.0, 0.0, 0.0, 1.0);
-		basicShader.setUniform3f("u_Color", 1, 0.3, 0.1);
-		basicShader.setUniform1f("u_Time", performance.now());
-		gl.clear(gl.COLOR_BUFFER_BIT);
-		gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, 0);
+	let lastTime = 0;
+	const FPS = 10; // Desired frames per second
+
+	const draw = (time: number) => {
+		const deltaTime = time - lastTime;
+		const interval = 1000 / FPS; // Interval in milliseconds for desired FPS
+
+		if (deltaTime >= interval) {
+			lastTime = time - (deltaTime % interval);
+
+			// Your WebGL setup code here (like clearing the buffer, setting uniforms, etc.)
+			gl.clear(gl.COLOR_BUFFER_BIT);
+			gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+			basicShader.setUniform3f(
+				"u_Color",
+				Math.random(),
+				Math.random(),
+				Math.random()
+			);
+			basicShader.setUniform1f("u_Time", performance.now());
+
+			// Your drawing code here
+			gl.drawElements(gl.TRIANGLES, 6, gl.UNSIGNED_INT, 0);
+		}
+
+		requestAnimationFrame(draw);
 	};
 
-	draw();
+	requestAnimationFrame(draw);
 })();
